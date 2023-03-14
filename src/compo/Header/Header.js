30d1f167c0c94d2e120d69af0dd69312f097a301
Aashishchoudhary <33976894+Aashishchoudhary@users.noreply.github.com>
2023-03-14 18:05:15 +0530

    second_push

--- src/compo/Header/Header.js
+++ src/compo/Header/Header.js
@@ -14,12 +14,16 @@ export default function Header() {
     setDis(false)
   }
  }
+ const bdycl=()=>{
+ setDis(false)
+  console.log('assaas')
+ }
   return (
     <>
-      <ul className='header'>
+      <ul className='header' >
       
-      <li className="header_icons"><NavLink   to='/' className='header_links'><img className='header_logo' src='https://cdn.indiawealth.in/cdn/images/fe/IND_logo_circle.png'></img></NavLink > </li>
-      <li className="header_icons"><input className='header_input' placeholder='Search'  type="text" name="" id="" /></li>
+      <li className="header_icons"><NavLink onClick={bdycl}  to='/' className='header_links'><img className='header_logo' src='https://cdn.indiawealth.in/cdn/images/fe/IND_logo_circle.png'></img></NavLink > </li>
+      <li className="header_icons"><input onClick={bdycl} className='header_input' placeholder='Search'  type="text" name="" id="" /></li>
 
 
 
@@ -45,12 +49,12 @@ export default function Header() {
 
 
       {/* <li className="header_icons"><Link onClick={props.investClick}  className='header_links'>Invest</Link > </li> */}
-      <li className="header_icons"><NavLink     to='/features' className='header_links'>Features</NavLink  ></li>
-      <li className="header_icons"><NavLink   to='/pricing' className='header_links'>Pricing</NavLink > </li>
-      <li className="header_icons"><NavLink   to='/blog' className='header_links'>Blog</NavLink > </li>
-      <li className="header_icons"><NavLink   to='/support' className='header_links'><SupportAgentIcon/></NavLink > </li>
-      <li className="header_icons login"><NavLink   to='/login' className='header_links'>Login</NavLink > </li>
-      <li className="header_icons  signup"><NavLink   to='/signup' className='header_links '>Sign Up</NavLink > </li>
+      <li onClick={bdycl} className="header_icons"><NavLink     to='/features' className='header_links'>Features</NavLink  ></li>
+      <li onClick={bdycl} className="header_icons"><NavLink   to='/pricing' className='header_links'>Pricing</NavLink > </li>
+      <li onClick={bdycl} className="header_icons"><NavLink   to='/blog' className='header_links'>Blog</NavLink > </li>
+      <li onClick={bdycl} className="header_icons"><NavLink   to='/support' className='header_links'><SupportAgentIcon/></NavLink > </li>
+      <li  onClick={bdycl} className="header_icons login"><NavLink   to='/login' className='header_links'>Login</NavLink > </li>
+      <li onClick={bdycl}  className="header_icons  signup"><NavLink   to='/signup' className='header_links '>Sign Up</NavLink > </li>
     </ul>
    
     </>
